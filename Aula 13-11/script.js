8fa3dc2e3623cd70e232d9f9b164e88596c4906f
Fariasnacla <clarafariias@gmail.com>
2023-11-17 21:39:23 -0300

    Aula dia 13 e dia 17
    dia 13(card, receita e icone)
    dia 17(figuras no css)

--- Aula 13-11/script.js
+++ Aula 13-11/script.js
@@ -8,14 +8,33 @@ card.setAttribute('class', 'card')
 
 let div_img = document.createElement('div')
 div_img.setAttribute('class', 'div-img')
+div_img.style.backgroundImage = "url('receita.jpg')"
 
 card.append(div_img)
 
 let div_chef = document.createElement('div')
 div_chef.setAttribute('class', 'chef-avatar')
+div_chef.style.backgroundImage = "url('helezarizzo')"
 
 card.append(div_chef)
 
+let btn_receita = document.createElement('button')
+btn_receita.setAttribute('class', 'btn-receita')
+btn_receita.textContent = "VER RECEITA"
+
+card.append(btn_receita)
+
+let lb_chef = document.createElement('label')
+lb_chef.setAttribute('class', 'lb-chef')
+lb_chef.textContent = "Nome do Chef"
+
+card.append(lb_chef)
+
+let lb_avaliacao = document.createElement('label')
+lb_avaliacao.setAttribute('class', 'lb-avaliacao')
+lb_avaliacao.textContent = "❤ 1.511"
+
+card.append(lb_avaliacao)
 
 fragment.append(card)
 
