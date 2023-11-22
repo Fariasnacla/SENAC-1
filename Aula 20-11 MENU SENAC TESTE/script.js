bf6d404f25ef9e70953ffebfb33477d3df287f99
Fariasnacla <clarafariias@gmail.com>
2023-11-21 21:32:29 -0300

    Dia 21-11 MENU
    Fiz alteração das divs e responsividade

---
+++ Aula 20-11 MENU SENAC TESTE/script.js
@@ -0,0 +1,12 @@
+function Toggle() {
+    let div_btns = document.getElementById("btns")
+
+    if (div_btns.style.display === "none") {
+        div_btns.style.display = "flex"
+    } else {
+        div_btns.style.display = "none"
+    }
+
+   
+
+
